67219716ec23f88c5ede70273a76582b9d5d3862
remrama <mallett.remy@gmail.com>
2022-12-03 22:56:28 -0600

    pypi setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# krank
+
+Steal sleep and dreams from other researchers.
+
+Uses [Pooch](https://www.fatiando.org/pooch) for simple and reliable downloading. Similar to [Ensaio](https://www.fatiando.org/ensaio) but for sleep.
---
+++ krank/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+# Define krank logger.
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S"
+)
+
+__author__  = "Remington Mallett <mallett.remy@gmail.com>"
+__version__ = "0.0.1"
